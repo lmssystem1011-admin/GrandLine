fbb2df0d7f069b9aa0dc9f4a9661ffd447322d9c
LMS Admin <lmssystem1011@gmail.com>
2025-08-25 09:15:31 +0530

    Test code
    In this test code for the model that  we can create

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ label, conf, probs = infer_image(test_img, enhanced_model, class_names)
 nsfw_score = compute_enhanced_nsfw_score(Image.open(test_img).convert("RGB"))
 
 if nsfw_score > 0.7:
-    print("⚠ Classified as Adult Content (NSFW)")
+    print("⚠️ Classified as Adult Content (NSFW)")
 else:
     print(f"Classified as {label} with confidence {conf * 100:.2f}%")
 
